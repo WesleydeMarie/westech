a859e0c1ca75ae682e2134e19160f815ab1fba8f
Wesley de Marie <wesleyf822@gmail.com>
2026-05-14 13:07:30 +0200

    Fixed: Updated index.html with Techwes branding and blue theme

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+<!DOCTYPE html>
+<html lang="nl">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#0066cc" />
+    <meta name="description" content="Techwes - Microsoft MVP Journey: Data Governance, Purview, en Security insights door Wesley de Marie" />
+    <meta name="author" content="Wesley de Marie" />
+    
+    <meta property="og:title" content="Techwes - Microsoft MVP Journey" />
+    <meta property="og:description" content="Data Governance, Purview, en Security insights door Wesley de Marie" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://techwes.nl" />
+    
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="Techwes - Microsoft MVP Journey" />
+    <meta name="twitter:description" content="Data Governance, Purview, en Security insights door Wesley de Marie" />
+    
+    <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+    
+    <title>Techwes - Microsoft MVP Journey</title>
+  </head>
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root"></div>
+  </body>
+</html>
